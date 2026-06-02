6818d669aa2c2ed464cf11563fa208b4746fca8d
intlb2b-dotcom <intl.b2b@gmail.com>
2026-06-03 00:15:02 +0800

    Add files via upload

---
+++ enable_pages.ps1
@@ -0,0 +1,2 @@
+= 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;
+=@{source=@{branch=gh-pages; path=/}} | ConvertTo-Json;
---
+++ enable_pages3.ps1
@@ -0,0 +1,4 @@
+=\" "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\  ;
+=@{Authorization=\Bearer" "\+; Accept=\application/vnd.github+json\};  ;
+=@{source=@{branch=\gh-pages\; path=\/\}} | ConvertTo-Json;  ;
+Invoke-RestMethod -Uri \https://api.github.com/repos/intlb2b-dotcom/Novosin/pages\ -Method Put -Headers  -Body  | ConvertTo-Json 
